f4b224b0c7a16c2c3abb6724bc9e1012511728b5
hentainer <144581058+hentainer@users.noreply.github.com>
2021-08-10 07:11:44 +0000

    some linting

--- app.go
+++ app.go
@@ -12,3 +12,13 @@ type Application struct {
 
 	Logger log15.Logger
 }
+
+// NewApplication creates an instance of Application.
+func NewApplication() *Application {
+	return &Application{}
+}
+
+// Close gracefully shuts down the application
+func (a *Application) Close() {
+	if a.Store != nil {
+		a.St
