9ab9dbf2719beaacd5825f886707c9f3efa612d4
hentainer <144581058+hentainer@users.noreply.github.com>
2021-09-02 18:26:49 +0000

    1.1.6

--- cmd/kage/server.go
+++ cmd/kage/server.go
@@ -10,4 +10,18 @@ import (
 	"time"
 
 	"github.com/msales/kage"
-	"github.com/msales/kage/se
+	"github.com/msales/kage/server"
+	"gopkg.in/urfave/cli.v1"
+)
+
+func runServer(c *cli.Context) {
+	app, err := newApplication(c)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer app.Close()
+
+	monitorTicker := time.NewTicker(30 * time.Second)
+	defer monitorTicker.Stop()
+	go func() {
+		for rang
