770e094cbd835716439762548861d5b147f7d831
hentainer <144581058+hentainer@users.noreply.github.com>
2022-01-13 15:34:16 +0000

    add a .gitignore file

--- interfaces.go
+++ interfaces.go
@@ -1,4 +1,13 @@
 package kage
 
 import (
-	"github.com/msales/
+	"github.com/msales/kage/kafka"
+	"github.com/msales/kage/store"
+)
+
+// Store represents an offset store.
+type Store interface {
+	// SetState adds a state into the store.
+	SetState(interface{}) error
+
+	// BrokerOffsets returns a snapshot of the current br
