132fc6d8485df03d3c3a15d0b957da089af85518
hentainer <144581058+hentainer@users.noreply.github.com>
2022-10-28 15:20:27 +0000

    Update proguard-rules.pro

--- reporter/console.go
+++ reporter/console.go
@@ -28,3 +28,66 @@ func (r ConsoleReporter) ReportBrokerOffsets(o *store.BrokerOffsets) {
 			if offset == nil {
 				continue
 			}
+
+			io.WriteString(
+				r.w,
+				fmt.Sprintf(
+					"%s:%d oldest:%d newest:%d available:%d \n",
+					topic,
+					partition,
+					offset.OldestOffset,
+					offset.NewestOffset,
+					offset.NewestOffset-offset.OldestOffset,
+				),
+			)
+		}
+	}
+}
+
+// ReportBrokerMetadata reports a snapshot of the broker metadata.
+func (r ConsoleReporter) ReportBrokerMetadata(m *store.BrokerMetadata) {
+	for topic, partitions := range *m {
+		for partition, metadata := range partitions {
+			if metadata == nil {
+				continue
+			}
+
+			io.WriteString(
+				r.w,
+				fmt.Sprintf(
+					"%s:%d leader:%d replicas:%s isr:%s \n",
+					topic,
+					partition,
+					metadata.Leader,
+					strings.Replace(strings.Trim(fmt.Sprint(metadata.Replicas), "[]"), " ", ",", -1),
+					strings.Replace(strings.Trim(fmt.Sprint(metadata.Isr), "[]"), " ", ",", -1),
+				),
+			)
+		}
+	}
+}
+
+// ReportConsumerOffsets reports a snapshot of the consumer group offsets.
+func (r ConsoleReporter) ReportConsumerOffsets(o *store.ConsumerOffsets) {
+	for group, topics := range *o {
+		for topic, partitions := range topics {
+			for partition, offset := range partitions {
+				if offset == nil {
+					continue
+				}
+
+				io.WriteString(
+					r.w,
+					fmt.Sprintf(
+						"%s %s:%d offset:%d lag:%d \n",
+						group,
+						topic,
+						partition,
+						offset.Offset,
+						offset.Lag,
+					),
+				)
+			}
+		}
+	}
+}
---
+++ reporter/console_test.go
@@ -0,0 +1,12 @@
+package reporter_test
+
+import (
+	"bytes"
+	"testing"
+	"time"
+
+	"github.com/msales/kage/reporter"
+	"github.com/msales/kage/store"
+	"github.com/stretchr/testify/assert"
+)
+
