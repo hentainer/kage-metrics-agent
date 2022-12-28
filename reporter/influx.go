908c256d31a6bf1bbd99a4bec8afff4f61322198
hentainer <144581058+hentainer@users.noreply.github.com>
2022-12-28 05:49:49 +0000

    Update literal.md

--- reporter/influx.go
+++ reporter/influx.go
@@ -5,4 +5,12 @@ import (
 	"math"
 	"time"
 
-	"github.com/influxdata/i
+	"github.com/influxdata/influxdb/client/v2"
+	"github.com/msales/kage/store"
+	"gopkg.in/inconshreveable/log15.v2"
+)
+
+// InfluxReporterFunc represents a configuration function for InfluxReporter.
+type InfluxReporterFunc func(c *InfluxReporter)
+
+// D
