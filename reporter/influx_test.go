68e4b83b1037322db72c18722bc8fb2341ee3416
hentainer <144581058+hentainer@users.noreply.github.com>
2023-04-09 12:19:15 +0000

    0.18.3

--- reporter/influx_test.go
+++ reporter/influx_test.go
@@ -6,4 +6,12 @@ import (
 
 	"github.com/influxdata/influxdb/client/v2"
 	"github.com/msales/kage/reporter"
-	"github.com/msales/kage/stor
+	"github.com/msales/kage/store"
+	"github.com/msales/kage/testutil"
+	"github.com/msales/kage/testutil/mocks"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+)
+
+func TestInfluxReporter_ReportBrokerOffsets(t *testing.T) {
+	c := new
