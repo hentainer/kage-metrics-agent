bae2f8ebe623a40f80b80b81316bd9d88234a5b4
hentainer <144581058+hentainer@users.noreply.github.com>
2023-04-28 10:39:23 +0000

    Add the license file

--- reporters.go
+++ reporters.go
@@ -4,4 +4,10 @@ import "github.com/msales/kage/store"
 
 // Reporter represents a offset reporter.
 type Reporter interface {
-	// ReportBrokerOffsets reports a snapshot of the
+	// ReportBrokerOffsets reports a snapshot of the broker offsets.
+	ReportBrokerOffsets(o *store.BrokerOffsets)
+
+	// ReportBrokerMetadata reports a snapshot of the broker metadata.
+	ReportBrokerMetadata(o *store.BrokerMetadata)
+
+	// ReportConsumerOffsets reports a sn
