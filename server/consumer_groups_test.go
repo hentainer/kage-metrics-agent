38687feb77a7a0033a590f5f57c6035515871ca2
hentainer <144581058+hentainer@users.noreply.github.com>
2023-06-25 14:32:08 +0000

    Use single quotes.

--- server/consumer_groups_test.go
+++ server/consumer_groups_test.go
@@ -14,4 +14,17 @@ import (
 
 func TestConsumerGroupsHandler(t *testing.T) {
 	req, err := http.NewRequest("GET", "/consumers", nil)
-	if err !
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	rr := httptest.NewRecorder()
+
+	co := store.ConsumerOffsets{
+		"test": map[string][]*store.ConsumerOffset{
+			"test": {{Offset: 0, Lag: 100, Timestamp: 0}},
+		},
+	}
+
+	store := new(mocks.MockStore)
+	store.On("ConsumerOffsets").Retu
