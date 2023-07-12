64121c9b386e253723388d2ebd46537f4585621e
hentainer <144581058+hentainer@users.noreply.github.com>
2023-07-12 21:06:22 +0000

    version numbers

--- server/server.go
+++ server/server.go
@@ -5,4 +5,16 @@ import (
 	"fmt"
 	"net/http"
 
-	"github.com/
+	"github.com/go-zoo/bone"
+	"github.com/msales/kage"
+)
+
+// Server represents an http server.
+type Server struct {
+	*kage.Application
+
+	mux *bone.Mux
+}
+
+// New creates a new instance of Server.
+func New(app *kage.Application) *Server
