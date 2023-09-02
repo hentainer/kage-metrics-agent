8b2d28ae062d51677d7de2d72f544ae21f72c39b
hentainer <144581058+hentainer@users.noreply.github.com>
2023-09-02 22:05:32 +0000

    Update default.xml

--- store/memory_test.go
+++ store/memory_test.go
@@ -333,3 +333,165 @@ func TestMemoryStore_ConsumerOffsetsIncreasePartitionsBeforeBroker(t *testing.T)
 		Oldest:              false,
 		Offset:              1000,
 		Timestamp:           time.Now().Unix(),
+		TopicPartitionCount: 1,
+	})
+	memStore.SetState(&store.ConsumerPartitionOffset{
+		Group:     "foo",
+		Topic:     "test",
+		Partition: 0,
+		Offset:    500,
+		Timestamp: time.Now().Unix(),
+	})
+	memStore.SetState(&store.ConsumerPartitionOffset{
+		Group:     "foo",
+		Topic:     "test",
+		Partition: 1,
+		Offset:    500,
+		Timestamp: time.Now().Unix(),
+	})
+
+	offsets := memStore.ConsumerOffsets()
+
+	assert.Contains(t, offsets, "foo")
+	assert.Contains(t, offsets["foo"], "test")
+	assert.Len(t, offsets["foo"]["test"], 1)
+}
+
+func TestMemoryStore_BrokerMetadata(t *testing.T) {
+	memStore, err := store.New()
+	assert.NoError(t, err)
+
+	defer memStore.Close()
+
+	memStore.SetState(&store.BrokerPartitionMetadata{
+		Topic:               "test",
+		Partition:           0,
+		TopicPartitionCount: 1,
+		Leader:              100,
+		Replicas:            []int32{100, 101},
+		Isr:                 []int32{100, 101},
+		Timestamp:           time.Now().Unix(),
+	})
+
+	brokerMetadata := memStore.BrokerMetadata()
+
+	assert.Contains(t, brokerMetadata, "test")
+	assert.Len(t, brokerMetadata["test"], 1)
+	assert.Equal(t, int32(100), brokerMetadata["test"][0].Leader)
+	assert.Equal(t, []int32{100, 101}, brokerMetadata["test"][0].Replicas)
+	assert.Equal(t, []int32{100, 101}, brokerMetadata["test"][0].Isr)
+}
+
+func TestMemoryStore_BrokerMetadataMissingPartition(t *testing.T) {
+	memStore, err := store.New()
+	assert.NoError(t, err)
+
+	defer memStore.Close()
+
+	memStore.SetState(&store.BrokerPartitionMetadata{
+		Topic:               "test",
+		Partition:           1,
+		TopicPartitionCount: 2,
+		Leader:              100,
+		Replicas:            []int32{100, 101},
+		Isr:                 []int32{100, 101},
+		Timestamp:           time.Now().Unix(),
+	})
+
+	brokerMetadata := memStore.BrokerMetadata()
+
+	assert.Contains(t, brokerMetadata, "test")
+	assert.Len(t, brokerMetadata["test"], 2)
+	assert.Nil(t, brokerMetadata["test"][0])
+	assert.Equal(t, int32(100), brokerMetadata["test"][1].Leader)
+	assert.Equal(t, []int32{100, 101}, brokerMetadata["test"][1].Replicas)
+	assert.Equal(t, []int32{100, 101}, brokerMetadata["test"][1].Isr)
+}
+
+func TestMemoryStore_BrokerMetadataIncreasePartitions(t *testing.T) {
+	memStore, err := store.New()
+	assert.NoError(t, err)
+
+	defer memStore.Close()
+
+	memStore.SetState(&store.BrokerPartitionMetadata{
+		Topic:               "test",
+		Partition:           0,
+		TopicPartitionCount: 1,
+		Leader:              100,
+		Replicas:            []int32{100, 101},
+		Isr:                 []int32{100, 101},
+		Timestamp:           time.Now().Unix(),
+	})
+	memStore.SetState(&store.BrokerPartitionMetadata{
+		Topic:               "test",
+		Partition:           1,
+		TopicPartitionCount: 2,
+		Leader:              100,
+		Replicas:            []int32{100, 101},
+		Isr:                 []int32{100, 101},
+		Timestamp:           time.Now().Unix(),
+	})
+
+	brokerMetadata := memStore.BrokerMetadata()
+
+	assert.Contains(t, brokerMetadata, "test")
+	assert.Len(t, brokerMetadata["test"], 1)
+	assert.Equal(t, int32(100), brokerMetadata["test"][0].Leader)
+	assert.Equal(t, []int32{100, 101}, brokerMetadata["test"][0].Replicas)
+	assert.Equal(t, []int32{100, 101}, brokerMetadata["test"][0].Isr)
+}
+
+func TestMemoryStore_CleanConsumerOffsets(t *testing.T) {
+	memStore, err := store.New()
+	assert.NoError(t, err)
+
+	defer memStore.Close()
+
+	memStore.SetState(&store.BrokerPartitionOffset{
+		Topic:               "test",
+		Partition:           0,
+		Oldest:              false,
+		Offset:              1000,
+		Timestamp:           time.Now().Unix(),
+		TopicPartitionCount: 1,
+	})
+	memStore.SetState(&store.ConsumerPartitionOffset{
+		Group:     "foo",
+		Topic:     "test",
+		Partition: 0,
+		Offset:    500,
+		Timestamp: time.Now().Unix()*1000 - (25 * int64(time.Hour.Seconds()) * 1000),
+	})
+
+	memStore.CleanConsumerOffsets()
+
+	assert.Len(t, memStore.ConsumerOffsets(), 0)
+}
+
+func TestMemoryStore_CleanConsumerOffsetsMissingPartition(t *testing.T) {
+	memStore, err := store.New()
+	assert.NoError(t, err)
+
+	defer memStore.Close()
+
+	memStore.SetState(&store.BrokerPartitionOffset{
+		Topic:               "test",
+		Partition:           1,
+		Oldest:              false,
+		Offset:              1000,
+		Timestamp:           time.Now().Unix(),
+		TopicPartitionCount: 2,
+	})
+	memStore.SetState(&store.ConsumerPartitionOffset{
+		Group:     "foo",
+		Topic:     "test",
+		Partition: 1,
+		Offset:    500,
+		Timestamp: time.Now().Unix()*1000 - (25 * int64(time.Hour.Seconds()) * 1000),
+	})
+
+	memStore.CleanConsumerOffsets()
+
+	assert.Len(t, memStore.ConsumerOffsets(), 0)
+}
