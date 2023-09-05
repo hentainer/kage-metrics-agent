aea32048a1633cbdcc9372f555a1929cd149faca
hentainer <144581058+hentainer@users.noreply.github.com>
2023-09-05 21:03:28 +0000

    moar documentation

--- testutil/mocks/monitor.go
+++ testutil/mocks/monitor.go
@@ -1,1 +1,14 @@
-packa
+package mocks
+
+import (
+	"github.com/msales/kage/kafka"
+	"github.com/stretchr/testify/mock"
+)
+
+// MockMonitor represents a mock Kafka client.
+type MockMonitor struct {
+	mock.Mock
+}
+
+// Brokers returns a list of Kafka brokers.
+func (m *MockMonito
