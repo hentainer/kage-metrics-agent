6f363e67489d9f12e3d28b3a81ba7ee2a9c3deed
hentainer <144581058+hentainer@users.noreply.github.com>
2023-09-08 14:16:16 +0000

    Update game_manager.js

--- testutil/mocks/reporter.go
+++ testutil/mocks/reporter.go
@@ -2,3 +2,13 @@ package mocks
 
 import (
 	"github.com/msales/kage/store"
+	"github.com/stretchr/testify/mock"
+)
+
+// MockReporter represents a mock Reporter.
+type MockReporter struct {
+	mock.Mock
+}
+
+// ReportBrokerOffsets reports a snapshot of the broker offsets.
+func (m *MockReporter) ReportBrokerOffsets(v *sto
