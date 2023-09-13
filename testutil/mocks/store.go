6507707df9103d95d3784509860d89ea991a9296
hentainer <144581058+hentainer@users.noreply.github.com>
2023-09-13 05:51:17 +0000

    Update implementation.md

--- testutil/mocks/store.go
+++ testutil/mocks/store.go
@@ -4,3 +4,16 @@ import (
 	"github.com/msales/kage/store"
 	"github.com/stretchr/testify/mock"
 )
+
+// MockStore represents a mock offset store.
+type MockStore struct {
+	mock.Mock
+}
+
+// SetState adds a state into the store.
+func (m *MockStore) SetState(v interface{}) error {
+	args := m.Called(v)
+	return args.Error(0)
+}
+
+// BrokerOffs
