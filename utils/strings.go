89e892dc90b3122e9ed13184e0daf3384e78c899
hentainer <144581058+hentainer@users.noreply.github.com>
2023-09-18 05:54:01 +0000

    recovering from fetal mistake

--- utils/strings.go
+++ utils/strings.go
@@ -4,4 +4,15 @@ import "strings"
 
 // SplitMap splits a slice of strings into a map of strings using
 // the given separator.
-func SplitMap(s []string, sep stri
+func SplitMap(s []string, sep string) map[string]string {
+	if len(s) == 0 || sep == "" {
+		return nil
+	}
+	m := make(map[string]string)
+	for _, str := range s {
+		parts := strings.SplitN(str, sep, 2)
+		v := ""
+		if len(parts) > 1 {
+			v = parts[1]
+		}
+		m[p
